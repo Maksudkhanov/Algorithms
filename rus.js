f4a2698903510dc604b0f78381406433339179e2
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2021-05-15 01:28:39 +0500

    Deleted rus.js

--- rus.js
+++
@@ -1,7 +0,0 @@
-function onOpen(e) {
-    // Add a custom menu to the spreadsheet.
-    SpreadsheetApp.getUi() // Or DocumentApp, SlidesApp, or FormApp.
-        .createMenu('Custom Menu')
-        .addItem('First item', 'menuItem1')
-        .addToUi();
-  }
